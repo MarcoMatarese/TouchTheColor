4247a8664bd823516459b5ea8c356dc518e208a6
Marco Matarese <marco.matarese@studenti.unina.it>
2019-05-27 12:38:53 +0200

    Added files.

---
+++ Colors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+
+class Colors:
+    colorNames = []
+    kinectValues = []
+
+    colorNames.append('BLUE')
+    colorNames.append('MAGENTA')
+    colorNames.append('LIGHT BLUE')
+    colorNames.append('LIGHT GREEN')
+
+    kinectValues.append((np.array([115,50,50]),np.array([125,255,255])))
+    kinectValues.append((np.array([145,50,50]),np.array([160,255,255])))
+    kinectValues.append((np.array([70, 50, 50]), np.array([110, 255, 255])))
+    kinectValues.append((np.array([38, 50, 50]), np.array([48, 255, 255])))
+
+def getColor(colorName):
+    colorID = Colors.colorNames.index(colorName)
+    return Colors.kinectValues[colorID]
---
+++ Vector2D.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from math import sqrt
+
+class Vector2D:
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+        self.infoCalculated = False
+
+    def calculateInfo(self):
+        self.intensity = sqrt(self.x**2 + self.y**2)
+        if self.intensity!=0.0:
+            self.versorX = self.x/self.intensity
+            self.versorY = self.y/self.intensity
+        else:
+            self.versorX = 0.0
+            self.versorY = 0.0
+        self.infoCalculated = True
+    
+    def __add__(self, other):
+        return Vector2D(self.x+other.x, self.y+other.y)
+
+    def __sub__(self, other):
+        return Vector2D(self.x-other.x, self.y-other.y)
+
+    def __str__(self):
+        formatParameters = (self.x, self.y, self.getIntensity(), self.getVersorX(), self.getVersorY())
+        return "Vector:(%.5f,%.5f) | Intensity: %.5f | Versor:(%.5f,%.5f)" % formatParameters
+
+    def getIntensity(self):
+        if not(self.infoCalculated):
+            self.calculateInfo()
+        return self.intensity
+    
+    def getVersorX(self):
+        if not(self.infoCalculated):
+            self.calculateInfo()
+        return self.versorX
+    
+    def getVersorY(self):
+        if not(self.infoCalculated):
+            self.calculateInfo()
+        return self.versorY
+
+    def setX(self, x):
+        self.x = x
+        self.infoCalculated = False
+
+    def setY(self, y):
+        self.y = y
+        self.infoCalculated = False
+
+    def multiplyMatrix(self, matrix):
+        x = self.x*matrix[0][0] + self.y*matrix[0][1]
+        y = self.x*matrix[1][0] + self.y*matrix[1][1]
+        ret = Vector2D(x,y)
+        return ret
